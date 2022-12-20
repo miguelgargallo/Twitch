b898a1dd20f3a6b8552ce2975e2fc2df00482121
Miguel Gargallo <mgargallo16@gmail.com>
2022-12-20 13:37:53 +0100

    1.0..5 Breadcrumb

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -8,9 +8,46 @@ export default function Home() {
         <title>Twitch - Mi pagina web</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
-        <Menu />
+      <main className="p-2">
+        <div
+          className="hidden sm:block m-2 py-8 px-8"
+        >
+          <Menu />
+          <div className="flex flex-auto m-2">
+            <p className="px-1 box-decoration-slice bg-gradient-to-r from-blue-500 to-blue-500 text-white">
+              Mi pagina web
+            </p>
+          </div>
+        </div>
+        <div className="m-2 py-8 px-8">
+          <h2 className="text-white text-xl">
+            Hola mundo, soy Miguel Gargallo, desarrollador de software.
+          </h2>
+        </div>
+        <div className="flex columns-2 text-white">
+          <div className="k m-2 py-8 px-8">
+            <h3 className="text-xl">
+              Educación y experiencia
+            </h3>
+            <p
+              className="text-sm py-2"
+            >
+              Soy un desarrollador de software con más de 10 años de experiencia
+              en el sector.
+            </p>
+          </div>
+          <div className="p-2">
+            <h3 className="text-xl">
+              Educación y experiencia
+            </h3>
+            <p
+              className="text-sm py-2"
+            >
+              Soy un desarrollador de software con más de 10 años de experiencia
+              en el sector.
+            </p>
+          </div>
+        </div>
       </main>
     </div>
   );
