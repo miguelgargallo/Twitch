6b54926d33138579e09d0a3a8f929d3df03fb186
Miguel Gargallo <mgargallo16@gmail.com>
2022-12-20 14:19:20 +0100

    1.0..7 Site day 1

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,32 +1,5 @@
 import * as React from "react";
-// Buttons
-export * from "./Button";
-// Menu
-export * from "./Menu";
-export * from "./Navbar";
-export * from "./Supermenu";
+
 export * from "./Breadcrumb";
-// Title and subtitle
-export * from "./TitlePylar";
-// List
-export * from "./ListPylar";
-// Version
-export * from "./Version";
-// Footer
-export * from "./Footer";
-// Other
-export * from "./Scrollable";
-/* Blog Pylar */
-export * from "./BlogPylar/blog";
-export * from "./BlogPylar/post12032022";
-/* Sales Pylar */
 export * from "./Contact";
-/* Investors Pylar */
-export * from "./Letter";
-/* Texts */
-export * from "./Salestext";
-/* Logos */
-export * from "./PylarLogo";
-export * from "./SuperdataLogo";
-export * from "./PencilLogo";
-export * from "./Carta";
+export * from "./Menu";
